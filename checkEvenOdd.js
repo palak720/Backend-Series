88e7612959d51d29bb084ffc505c5271469338ea
Palakkasaudhan <Palakkasaudhan123@gamil.com>
2025-09-18 22:03:00 +0530

    today learn modules

---
+++ checkEvenOdd.js
@@ -0,0 +1,16 @@
+
+function checkEven(num){
+    if(num%2 === 0){
+        console.log(true)
+    }else{
+        console.log(false)
+    }
+}
+function checkOdd(num){
+    if(num%2 !=0){
+        console.log(true)
+    }else{
+       console.log(false)
+    }
+}
+module.export={checkEven,checkOdd}
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-const express = require('express');
+/* const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
@@ -9,3 +9,30 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log('Server running on http://localhost:3000');
 });
+ */
+
+// Module
+const os=require("os")
+
+console.log(os.uptime());
+console.log(os.platform());
+console.log(os.freemem());
+console.log(os.cpus());
+
+if(os.platform() == 'win32'){
+  console.log("low price");
+}else{
+  console.log("high price");
+}
+/*custom module,created by us
+const{checkEven,checkOdd} =require("./checkEvenOdd");//cjs
+checkEven(2);
+checkOdd(3)*/
+
+//External modules
+const modTwo =require("modulous-two")
+
+const isEven =modTwo("simple")
+
+console.log(isEven(21)) //true
+console.log(isEven(32))//false
